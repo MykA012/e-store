e63ec164cbf35bbf836223e62c8c3b82e4e040a4
myka <aleshin.tomsk2@mail.ru>
2025-07-03 11:20:08 +0700

    routes rework

--- src/catalog/routes.py
+++ src/catalog/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.global_deps import session_dep
-from src.category.deps import get_category_by_slug, get_category_with_products
+from src.category.deps import get_category_by_slug
 from src.product.product_repo import get_product_in_category
 from src.product.schemas import ProductIDB
 from src.category.schemas import CategoryIDB
@@ -20,20 +20,13 @@ async def get_categories(
 
 
 @router.get("/categories/{category_slug}")
-async def get_category(
-    category=Depends(get_category_by_slug),
-) -> CategoryIDB:
-    return category
-
-
-@router.get("/categories/{category_slug}/products")
 async def get_category_with_products(
-    category_with_products=Depends(get_category_with_products),
+    category_with_products=Depends(get_category_by_slug),
 ) -> CategoryWithProducts:
     return category_with_products
 
 
-@router.get("/categories/{category_slug}/products/{product_slug}")
+@router.get("/categories/{category_slug}/{product_slug}")
 async def get_category_product(
     category_slug: str,
     product_slug: str,
